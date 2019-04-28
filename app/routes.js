02818e919b154c9cb527772aadc9e65981ca7b53
Eve Bayram <eve.bayram@faststream.civilservice.gov.uk>
2019-04-28 17:34:11 +0100

    force start page

--- app/routes.js
+++ app/routes.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const router = express.Router()
-
+router.get('/', (req, res) => {
+  res.redirect('/start')
+})
 // Add your routes here - above the module.exports line
 
 module.exports = router
